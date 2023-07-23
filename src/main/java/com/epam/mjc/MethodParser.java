4bfabf49e395285ff4a8e899a6528fc9c473f62c
yuliyago <yuuliya02@proton.me>
2023-07-23 20:14:20 +0300

    String task has been done.

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,52 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String[] stringsSeparatedByOpeningBrace = signatureString.split("[(]");
+        String[] stringsBeforeOpeningBrace = stringsSeparatedByOpeningBrace[0].split(" ");
+        String accessModifier = "", returnType, methodName;
+        if (stringsBeforeOpeningBrace.length == 3) {
+            accessModifier = stringsBeforeOpeningBrace[0];
+            returnType = stringsBeforeOpeningBrace[1];
+            methodName = stringsBeforeOpeningBrace[2];
+        } else {
+            returnType = stringsBeforeOpeningBrace[0];
+            methodName = stringsBeforeOpeningBrace[1];
+        }
+        String[] arguments = stringsSeparatedByOpeningBrace[1].replace(")", "").split(", ");
+        List<MethodSignature.Argument> listOfArguments = new ArrayList<>();
+        if (arguments.length != 1) {
+            for (String argument : arguments) {
+                String[] strings = argument.split(" ");
+                listOfArguments.add(new MethodSignature.Argument(strings[0], strings[1]));
+            }
+        }
+        MethodSignature methodSignature = new MethodSignature(methodName, listOfArguments);
+        if (accessModifier.length() != 0) {
+            methodSignature.setAccessModifier(accessModifier);
+        }
+        methodSignature.setReturnType(returnType);
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,42 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       List<String> substrings = new ArrayList<>();
+       int startPos=0, endPos;
+       while(startPos<source.length()){
+           endPos=source.length();
+           for (String s:delimiters) {
+               int index = source.indexOf(s,startPos);
+               if(index!=-1 && index<endPos){
+                   endPos=index;
+               }
+           }
+           if(endPos!=source.length()){
+               if(!source.substring(startPos,endPos).equals("")){
+                   substrings.add(source.substring(startPos,endPos));
+               }
+               startPos=endPos+1;
+           }
+           else{
+               if(!source.substring(startPos).equals("")){
+                   substrings.add(source.substring(startPos));
+               }
+               break;
+           }
+       }
+       return substrings;
     }
 }
